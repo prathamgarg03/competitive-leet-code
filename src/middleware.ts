8789e5f34d4b5280741ebe1e232b545beb2fe5fe
prathamgarg03 <gargpratham.312003@gmail.com>
2025-02-14 17:05:56 -0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/game(.*)'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/create-quiz(.*)', '/game(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect()
