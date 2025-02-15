dab380ed13ee7ac245ad0ab3d08db45b341e4e1c
prathamgarg03 <gargpratham.312003@gmail.com>
2025-02-14 22:48:24 -0800

    Update route.ts

--- src/app/api/create-user/route.ts
+++ src/app/api/create-user/route.ts
@@ -2,7 +2,7 @@ import {NextApiRequest} from "next";
 
 export async function POST(req: NextApiRequest) {
     const { type, data } = req.body;
-    if(type === "user-created") {
+    if(type === "user.created") {
         console.log("User created", data);
     }
 }
