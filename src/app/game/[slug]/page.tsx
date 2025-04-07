277129f84ed4b347804e363d7d988e1b5d5fd5ea
prathamgarg03 <gargpratham.312003@gmail.com>
2025-04-06 18:57:34 -0700

    Refactor DashboardPage component to enable client-side rendering; integrate CodeEditor and TerminalOutput components for enhanced user interaction.

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -1,34 +1,34 @@
-// "use client";
-// import { useState } from "react";
-// import CodeEditor from "@/components/CodeEditor";
-// import PowersPanel from "@/components/PowersPanel";
-// import { TerminalOutput } from "@/components/TerminalOutput";
-//
-//
-// export default async function DashboardPage({params,
-// }: {
-//     params: Promise<{ slug: string }>
-// }) {
-//     const [output, setOutput] = useState<string[]>(["Welcome to the terminal!"]);
-//     const slug = (await params).slug
-//     return (
-//         <main className="flex h-screen p-4">
-//             <div className="w-4/5 flex flex-col space-y-4">
-//                 <CodeEditor setOutput={setOutput} />
-//                 <TerminalOutput output={output} />
-//             </div>
-//
-//             <div className="w-1/5">
-//                 <PowersPanel />
-//             </div>
-//         </main>
-//     );
-// }
+"use client";
+import { useState } from "react";
+import CodeEditor from "@/components/CodeEditor";
+import PowersPanel from "@/components/PowersPanel";
+import { TerminalOutput } from "@/components/TerminalOutput";
 
-export default function GamePage() {
+
+export default function DashboardPage({params,
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    const [output, setOutput] = useState<string[]>(["Welcome to the terminal!"]);
+    // const slug = (await params).slug
     return (
-        <div>
-            Game Page
-        </div>
+        <main className="flex h-screen p-4">
+            <div className="w-4/5 flex flex-col space-y-4">
+                <CodeEditor setOutput={setOutput} />
+                <TerminalOutput output={output} />
+            </div>
+
+            <div className="w-1/5">
+                <PowersPanel />
+            </div>
+        </main>
     );
 }
+
+// export default function GamePage() {
+//     return (
+//         <div>
+//             Game Page
+//         </div>
+//     );
+// }
